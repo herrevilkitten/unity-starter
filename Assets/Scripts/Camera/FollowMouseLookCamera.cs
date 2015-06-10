f2c0f33e2c615ec42df29a821c8448eb2f0063b9
ekidder3 <eric.kidder@gtri.gatech.edu>
2015-06-10 08:06:05 -0400

    Initial commit

---
+++ Assets/Scripts/Camera/FollowMouseLookCamera.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FollowMouseLookCamera : MonoBehaviour
+{
+	public GameObject target;
+	public GameObject placeholder;
+	public Text statusText;
+	Vector3 offset;
+	float yPosition;
+	float xPosition;
+
+	// Use this for initialization
+	void Start ()
+	{
+		offset = target.transform.position - placeholder.transform.position;
+
+		placeholder.transform.LookAt (target.transform);
+
+		yPosition = placeholder.transform.eulerAngles.y;
+		xPosition = placeholder.transform.eulerAngles.x;
+
+		Quaternion rotation = Quaternion.Euler (yPosition, xPosition, 0);
+
+		transform.position = placeholder.transform.position;// - (rotation * offset);
+		transform.LookAt (target.transform);
+	}
+	
+	// Update is called once per frame	
+	void LateUpdate ()
+	{
+		bool looking = Input.GetButton ("Fire1");
+		float upDown = Input.GetAxis ("Mouse Y") * -5f;
+		float leftRight = Input.GetAxis ("Mouse X") * -5f;
+
+		// Use the placeholder to get the default angle to the target
+		// First, look at the target
+		placeholder.transform.LookAt (target.transform);
+		
+		// Then get the x and y angles
+		float desiredX = placeholder.transform.eulerAngles.x;
+		float desiredY = placeholder.transform.eulerAngles.y;
+
+		if (looking) {
+
+			yPosition = yPosition + upDown;//Mathf.Clamp (yPosition + upDown, -45f, 45f);//Mathf.Max (-10f, Mathf.Min (10f, yPosition + upDown));
+//		xPosition = Mathf.Clamp (xPosition + leftRight, -180f, 180f);//Mathf.Max (-10f, Mathf.Min (10f, yPosition + upDown));
+			xPosition = xPosition + leftRight;
+//		float desiredAngle = target.transform.eulerAngles.y;
+//		Quaternion rotation = Quaternion.Euler (yPosition, desiredAngle, 0);
+			Quaternion rotation = Quaternion.Euler (yPosition, xPosition, 0);
+			transform.position = target.transform.position - (rotation * offset);
+		} else {
+			yPosition = desiredY; //Mathf.Lerp (yPosition, desiredY, Time.deltaTime);
+			xPosition = desiredX; //Mathf.Lerp (xPosition, desiredX, Time.deltaTime);
+			transform.position = placeholder.transform.position;// target.transform.position - (rotation * offset);
+		}
+		
+		Debug.Log ("Mouse Y: " + upDown + " Mouse X: " + leftRight + "   " + placeholder.transform.position + "/" + placeholder.transform.eulerAngles + "   " + transform.position + "/" + transform.eulerAngles);
+		statusText.text = "Mouse Y: " + upDown + "\n"
+			+ "Mouse X: " + leftRight + "\n"
+			+ "MouseDown: " + looking + "\n"
+			+ "Follow Cam:\n   " + placeholder.transform.position + "/" + "\n   " + placeholder.transform.eulerAngles + "\n"
+			+ "Main Cam:\n   " + transform.position + "/" + "\n" + "   " + transform.eulerAngles + "\n";
+		
+		transform.LookAt (target.transform);
+	}
+}
---
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMovement : MonoBehaviour
+{
+	public float backwardsSpeed = 2.0f;
+	public float walkingSpeed = 3.0f;
+	public float runningSpeed = 7.5f;
+	public float turningSpeed = 60f;
+	public float accelerationSpeed = .3f;
+	public float movementMultiplier = 15f;
+
+	private float currentSpeed = 0.0f;
+	private float sidewaysSpeed = 0.0f;
+	private float direction = 1f;
+	private bool jumping = false;
+
+	Animator animator;
+	Rigidbody playerRigidBody;
+	CharacterController controller;
+
+	void Start ()
+	{
+		animator = GetComponent<Animator> ();
+		controller = GetComponent<CharacterController> ();
+	}
+
+	void Update ()
+	{
+		if (jumping) {
+			if (controller.isGrounded) {
+				jumping = false;
+				animator.SetBool ("IsJumping", false);
+			} else {
+				return;
+			}
+		}
+
+		if (Input.GetButton ("Jump")) {
+			animator.SetBool ("IsJumping", true);
+			jumping = true;
+			return;
+		}
+
+		// http://answers.unity3d.com/questions/543421/gradual-acceleration.html
+		Boolean running = Input.GetButton ("Run");
+
+		float rotate = Input.GetAxis ("Horizontal") * turningSpeed * Time.deltaTime;
+		if (running) {
+			rotate = rotate * 2f;
+		}
+		transform.Rotate (0, rotate, 0);
+		animator.SetFloat ("TSpeed", rotate);
+
+		float strafe = 0;
+		if (Input.GetButton ("Left Strafe")) {
+			strafe = -1;
+		} else if (Input.GetButton ("Right Strafe")) {
+			strafe = 1;
+		}
+		animator.SetFloat ("HSpeed", strafe);
+
+		float movement = Input.GetAxis ("Vertical");
+		Vector3 vector = Vector3.zero;
+		if (movement < 0f) {
+			// Going backwards
+			currentSpeed = Mathf.Min (backwardsSpeed, currentSpeed + accelerationSpeed);
+			direction = -1f;
+		} else if (movement > 0f) {
+			direction = 1f;
+			// Going forward
+			if (running) {
+				currentSpeed = Mathf.Min (runningSpeed, (currentSpeed + accelerationSpeed));
+				controller.slopeLimit = 60;
+			} else {
+				controller.slopeLimit = 45;
+				if (currentSpeed > walkingSpeed) {
+					// Decelerate
+					currentSpeed = Mathf.Max (walkingSpeed, currentSpeed - accelerationSpeed);
+				} else {
+					// Accelerate / maintain
+					currentSpeed = Mathf.Min (walkingSpeed, currentSpeed + accelerationSpeed);
+				}
+			}
+		} else {
+			// Player is not moving, so slow down to 0
+			currentSpeed = Mathf.Max (0f, (currentSpeed - accelerationSpeed));
+		}
+
+		if (direction > 0) {
+			vector = transform.TransformDirection (Vector3.forward);
+		} else {
+			vector = transform.TransformDirection (Vector3.back);
+		}
+
+		if (currentSpeed > 0f) {
+			controller.SimpleMove (vector * currentSpeed * Time.deltaTime * movementMultiplier);
+		} else {
+			direction = 1f;
+		}
+
+		animator.SetFloat ("VSpeed", currentSpeed * direction);
+		animator.SetBool ("IsMoving", currentSpeed != 0f || rotate != 0f);
+		animator.SetBool ("IsStrafing", strafe != 0f);
+
+		//Debug.Log ("Movement: " + controller.isGrounded + " Rotation: " + rotate + ", Speed: " + currentSpeed + "   " + movement + "   " + vector);
+	}
+}
